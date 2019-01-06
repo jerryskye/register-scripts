52088fbb99db378c1e27356ad9d0155a82d0221c
jerryskye <jarek@siedlarz.com>
2019-01-06 23:51:03 +0100

    Add device_id to entries request

--- entries.sh
+++ entries.sh
@@ -2,15 +2,16 @@
 
 API_URL="http://192.168.0.100:3000/entries.json"
 SECRET=`echo 'select secret from secret' | sqlite3 tokens.db`
-JWT_HEADER=`echo -n '{"typ": "JWT", "alg": "HS256"}' | base64`
+DEVICE_ID=`echo 'select device_id from secret' | sqlite3 tokens.db`
+JWT_HEADER=`echo -n '{"typ": "JWT", "alg": "HS256"}' | base64 -w 0`
 LOGFILE="`dirname $0`/curl.log"
 
 while true; do
   uid=`./read_uid`
   [ $? -ne 0 ] && echo "[`date`] Failed to read your card. Please try again or contact the system administrator if the problem persists." && continue
   exp="$((`date +%s` + 5))"
-  payload=`echo -n "{\"exp\": $exp}" | base64`
-  signature=`echo -n "$JWT_HEADER.$payload" | hmac256 --binary "$SECRET" | base64`
+  payload=`echo -n "{\"exp\": $exp, \"device_id\": \"$DEVICE_ID\"}" | base64 -w 0`
+  signature=`echo -n "$JWT_HEADER.$payload" | hmac256 --binary "$SECRET" | base64 -w 0`
 
   curl -s -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer $JWT_HEADER.$payload.$signature" -d "{\"uid\": \"$uid\"}" "$API_URL" >> $LOGFILE
   echo >> $LOGFILE
