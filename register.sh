63b2b091beb5b98454f7f243556fd3d21cfe2715
jerryskye <jarek@siedlarz.com>
2019-03-03 02:44:03 +0100

    Switch to JWT for registration tokens

--- register.sh
+++ register.sh
@@ -2,20 +2,14 @@
 
 REGISTER_URL='http://192.168.0.100:3000/users/sign_up'
 SECRET=`echo 'select secret from secret' | sqlite3 tokens.db`
-
-tokens_count=`echo 'select count(*) from registration_tokens' | sqlite3 tokens.db`
+JWT_HEADER=`echo -n '{"typ": "JWT", "alg": "HS256"}' | base64 -w 0`
 
 while true; do
-  if [ $tokens_count -lt 1 ]; then
-    echo Out of tokens!
-    exit
-  fi
   uid=`./read_uid`
   [ $? -ne 0 ] && echo "[`date`] Failed to read your card. Please try again or contact the system administrator if the problem persists." && continue
-  token=`echo 'select token from registration_tokens limit 1' | sqlite3 tokens.db`
-  hmac=`echo -n $token | hmac256 "$SECRET"`
-  echo "delete from registration_tokens where token='$token'" | sqlite3 tokens.db
-  ((tokens_count -= 1))
-  echo -n "$REGISTER_URL?uid=$uid&token=$token&hmac=$hmac" | qrencode -o qr.png
+  exp="$((`date +%s` + 86400))"
+  payload=`echo -n "{\"exp\": $exp, \"sub\": \"$uid\", \"admin\": false}" | base64 -w 0`
+  signature=`echo -n "$JWT_HEADER.$payload" | hmac256 --binary "$SECRET" | base64 -w 0`
+  echo -n "$REGISTER_URL?token=$JWT_HEADER.$payload.$signature" | qrencode -o qr.png
   timeout --foreground 8 fbi -a qr.png > /dev/null 2>&1
 done
